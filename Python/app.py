703011b0614e4fa9dd50f47bdab29e2f2d60240d
Duy anh <hoilamj22102007@gmail.com>
2025-10-20 19:38:06 +0700

    Auto update 2025-10-20 19:38:03

--- Python/app.py
+++ Python/app.py
@@ -1,9 +1,8 @@
 string = input("Enter a string: ")
-findnumber = []
-sum = 0
-for Characters in string:
-    if Characters.isdigit():
-        sum += int(Characters)
-        findnumber.append(Characters)
-print(f"The number of digits in the string is {findnumber}")
-print(f"The digits in the string are {sum}")
+def process_string(string):
+    string = " ".join(string.split()).lower()
+    string = string + "."
+    string = string.capitalize()
+    
+    return string
+print(process_string(string))
